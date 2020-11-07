7653e44f238abd60a4731206bfee62a5c8a8eb3e
Puranjoy Saha <p9saha@outlook.com>
2020-11-07 15:14:36 -0700

    add models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True)
+    name = models.CharField(max_feild=200, null=True)
+    email = models.CharField(max_feild=200, null=True)
